a1aa27ec45e79beb5170c46f49f0d425a372026e
stevenraykelly-arch <stevenraykelly@gmail.com>
2026-01-04 15:38:19 +1100

    fix: allow external hosts in Astro preview server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    server: {
+        host: true
+    }
 });
